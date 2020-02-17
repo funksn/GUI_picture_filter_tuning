24a42905fc26bac07b20870c35ace0818cc56c69
Stefan Funk <ukehs@student.kit.edu>
2020-02-17 22:04:22 +0100

    cleaned up repo

--- evaluate_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-
-manual_data = pd.DataFrame()
-
-for file in os.listdir(r'C:\Users\Stefan\bwSyncAndShare\LehrlaborEnergietechnik (Matthias Probst)\imgs\LaserDuenn_Vol250_pd200\Cam1_001A'):
-    if file.split('.') =='csv':
